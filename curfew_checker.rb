9242ca28cfe3a4e16c8376ba6d0cac8f3847df93
Dana Abdelhalim <dana.abdelhalim18@gmail.com>
2018-06-17 19:06:32 +0000

    Done.

---
+++ curfew_checker.rb
@@ -0,0 +1,27 @@
+def simple_curfew_checker (time)
+   if time = 11 || time > 11
+      puts "You're in trouble! Better get home quick!"
+   end
+end 
+
+def curfew_checker (time)
+   if time = 11 || time > 11
+      puts "You're in trouble! Better get home quick!"
+    else 
+      puts "Keep having fun!"
+    end
+end 
+
+
+def complex_curfew_checker (time)
+  if time == 11
+    puts "Time to apparate!"
+  elsif time == 12 
+    puts "You're in trouble! Better get home quick!"
+  elsif time < 11 
+    puts "Keep having fun!"
+  end 
+end 
+
+
+def 
